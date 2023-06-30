a30efefd0f2a9fe36d4dc7d776ef05a6b95351f4
Amaryann <121064694+Amaryann@users.noreply.github.com>
2023-06-30 21:33:24 +0300

    part one

---
+++ src/components/FriendList.js
@@ -0,0 +1,36 @@
+import PropTypes from 'prop-types';
+import { FriendListItem } from './FriendListItem';
+export const FriendList = ({friends}) =>{
+  return (
+    <ul>
+      {friends.map(friend => (
+        <FriendListItem
+          key={friend.id}
+          avatar={friend.avatar}
+          name={friend.name}
+          isOnline={friend.isOnline}
+          width="100"
+        />
+      ))}
+    </ul>
+  );
+}
+
+FriendList.propTypes = {
+friends: {
+  avatar:PropTypes.string,
+  name: PropTypes.string,
+  isONline: PropTypes.bool,
+  id: PropTypes.number,
+}
+}
+// FriendList.propTypes = {
+//   friends: PropTypes.arrayOf(
+//     PropTypes.shape({
+//       id: PropTypes.number,
+//       isOnline: PropTypes.bool,
+//       avatar: PropTypes.string,
+//       name: PropTypes.string,
+//     })
+//   ),
+// };
---
+++ src/components/FriendListItem.js
@@ -0,0 +1,15 @@
+export const FriendListItem = ({avatar, name, isOnline, id}) => {
+    return (
+        <li key={id}>
+            <span></span>
+            <img
+        
+        src={avatar}
+        alt="User avatar"
+        width="48"
+      />
+      <p>{name}</p>
+
+        </li>
+    )
+}
